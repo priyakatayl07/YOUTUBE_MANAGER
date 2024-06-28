d7ea21f40341f1dc9d766c1df1e99fbc6255c277
priya katyal <priyakatyal07@gmail.com>
2024-06-28 22:57:58 +0530

    api modify

--- api_handling_practice.py
+++ api_handling_practice.py
@@ -8,15 +8,15 @@ def fetch_youtube_videos():
         video_data = data["data"]
         video_title = video_data["video"]["items"]["snippet"]["title"]
         custom_url = video_data["channel"]["info"]["customUrl"]
-        
-        return video_title, custom_url
+        publish_at= video_data["channel"]["info"]["publishedAt"]
+        return video_title, custom_url,publish_at
     else:
         raise Exception("Failed to fetch data")
     
 def main():
     try:
-        video_title, custom_url = fetch_youtube_videos()
-        print(f"Video Title: {video_title}\nCustom URL: {custom_url}")
+        video_title, custom_url,publish_at = fetch_youtube_videos()
+        print(f"Video Title: {video_title}\nCustom URL: {custom_url}\n publishAT:{publish_at}")
     except Exception as e:
         print(f"Error: {str(e)}")
 
---
+++ youtube_manager_mongodb.py
@@ -0,0 +1,3 @@
+import pymongo
+
+pymongo.MongoClient("")
